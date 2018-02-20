19c2179bc1de3daa6effe38b17386bdecd839fa8
aaron <pleaseshutupaaron@gmail.com>
2018-02-19 19:35:47 -0500

    added design and README

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
   _getRandomIntInclusive(max) {
     var min = Math.ceil(1);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
   _rollDice(max) {
@@ -91,46 +91,53 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="holder" id="d4">
-          <Button className="roller" handleClick={() => this._rollDice(4)} id="d4-button" label="d4"/>
-          <Button className="clear" handleClick={() => this._clearRoll(4)} label="clear"/>
-          <div className="results" id="d4results">
-            {this._renderRolls(this.state.d4)}
+        <div className="mainBox">
+          <div className="header">
+            <h1>Roll for initiative</h1>
+            <p>Thanks for visiting my coding sample. I created this DnD dice roller app with Test Driven Development. You can find out more about my process in the README.</p>
+            <p>Please visit my GitHub or my Portfolio site.</p>
           </div>
-        </div>
-        <div className="holder" id="d6">
-          <Button className="roller" handleClick={() => this._rollDice(6)} id="d6-button" label="d6"/>
-          <Button className="clear" handleClick={() => this._clearRoll(6)} label="clear"/>
-          <div className="results" id="d6results">
-            {this._renderRolls(this.state.d6)}
+          <div className="holder" id="d4">
+            <Button className="roller" handleClick={() => this._rollDice(4)} id="d4-button" label="d4"/>
+            <Button className="clear" handleClick={() => this._clearRoll(4)} label="clear"/>
+            <div className="results" id="d4results">
+              {this._renderRolls(this.state.d4)}
+            </div>
           </div>
-        </div>
-        <div className="holder" id="d8">
-          <Button className="roller" handleClick={() => this._rollDice(8)} id="d8-button" label="d8"/>
-          <Button className="clear" handleClick={() => this._clearRoll(8)} label="clear"/>
-          <div className="results" id="d8results">
-            {this._renderRolls(this.state.d8)}
+          <div className="holder" id="d6">
+            <Button className="roller" handleClick={() => this._rollDice(6)} id="d6-button" label="d6"/>
+            <Button className="clear" handleClick={() => this._clearRoll(6)} label="clear"/>
+            <div className="results" id="d6results">
+              {this._renderRolls(this.state.d6)}
+            </div>
           </div>
-        </div>
-        <div className="holder" id="d10">
-          <Button className="roller" handleClick={() => this._rollDice(10)} id="d10-button" label="d10"/>
-          <Button className="clear" handleClick={() => this._clearRoll(10)} label="clear"/>
-          <div className="results" id="d10results">
-            {this._renderRolls(this.state.d10)}
+          <div className="holder" id="d8">
+            <Button className="roller" handleClick={() => this._rollDice(8)} id="d8-button" label="d8"/>
+            <Button className="clear" handleClick={() => this._clearRoll(8)} label="clear"/>
+            <div className="results" id="d8results">
+              {this._renderRolls(this.state.d8)}
+            </div>
           </div>
-        </div>
-        <div className="holder" id="d12">
-          <Button className="roller" handleClick={() => this._rollDice(12)} id="d12-button" label="d12"/>
-          <Button className="clear" handleClick={() => this._clearRoll(12)} label="clear"/>
-          <div className="results" id="d12results">
-            {this._renderRolls(this.state.d12)}
+          <div className="holder" id="d10">
+            <Button className="roller" handleClick={() => this._rollDice(10)} id="d10-button" label="d10"/>
+            <Button className="clear" handleClick={() => this._clearRoll(10)} label="clear"/>
+            <div className="results" id="d10results">
+              {this._renderRolls(this.state.d10)}
+            </div>
           </div>
-        </div>
-        <div className="holder" id="d20">
-          <Button className="roller" handleClick={() => this._rollDice(20)} id="d20-button" label="d20"/>
-          <Button className="clear" handleClick={() => this._clearRoll(20)} label="clear"/>
-          <div className="results" id="d20results">
-            {this._renderRolls(this.state.d20)}
+          <div className="holder" id="d12">
+            <Button className="roller" handleClick={() => this._rollDice(12)} id="d12-button" label="d12"/>
+            <Button className="clear" handleClick={() => this._clearRoll(12)} label="clear"/>
+            <div className="results" id="d12results">
+              {this._renderRolls(this.state.d12)}
+            </div>
+          </div>
+          <div className="holder" id="d20">
+            <Button className="roller" handleClick={() => this._rollDice(20)} id="d20-button" label="d20"/>
+            <Button className="clear" handleClick={() => this._clearRoll(20)} label="clear"/>
+            <div className="results" id="d20results">
+              {this._renderRolls(this.state.d20)}
+            </div>
           </div>
         </div>
       </div>
